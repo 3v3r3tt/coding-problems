8b6373bb2cdcc3d850c937796fb3bfc0aa9e5fd7
Paul M <paul@deyrd.com>
2016-07-20 15:37:00 -0600

    Adding solution to generate all permutations of a string

---
+++ Interview Cake Problems/stringPermutations.js
@@ -0,0 +1,18 @@
+function generatePermutations(inputString) {
+  if(inputString < 2) return new Set(inputString);
+
+  var allCharactersExceptLast = inputString.slice(0, -1);
+  var lastCharacter = inputString[inputString.length - 1];
+
+  var permutationsOfAllCharactersExceptLast = generatePermutations(allCharactersExceptLast);
+
+  var stringPermutations = new Set();
+  permutationsOfAllCharactersExceptLast.forEach(function(permutationOfAllCharactersExceptLast) {
+    for(let position=0; position<=permutationOfAllCharactersExceptLast.length; position++){
+      var permutation = permutationOfAllCharactersExceptLast.slice(0, position) + lastCharacter + permutationOfAllCharactersExceptLast.slice(position);
+      stringPermutations.add(permutation);
+    }
+  });
+
+  return stringPermutations;
+}
